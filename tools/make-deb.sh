1791ef7c7130d26c3c802cb4e8445c20b82300ae
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-02-21 05:04:34 +0300

    Further enhacement

--- tools/make-deb.sh
+++ tools/make-deb.sh
@@ -5,7 +5,7 @@ if ! dpkg -l | grep md5deep
 then
 	sudo apt-get install md5deep
 fi
-if ! dpkg -l | grep cmake
+if ! dpkg -l | grep debhelper
 then
 	sudo apt-get install debhelper
 fi
@@ -29,21 +29,26 @@ make
 
 echo -en "\033[37;1;41mCreate temporary directory for package and copy required files...\033[0m\n"
 cd ./tools/
+
+# Making catalog structure 
 mkdir -p ./kbe/usr/share/kbe
 mkdir -p ./kbe/usr/bin
 mkdir -p ./kbe/usr/share/applications
 mkdir -p ./kbe/usr/share/pixmaps
+
+# Copying needed files into catalog structure
 cp -r ./DEBIAN ./kbe/
 mv -f ../media ./kbe/usr/share/kbe/
 mv -f ../kbe ./kbe/usr/share/kbe/
-ln -s /usr/share/kbe/kbe ./kbe/usr/bin/kbe
-
 cp ./files/kbe.desktop ./kbe/usr/share/applications/
 cp ./files/kbe.xpm ./kbe/usr/share/pixmaps/
 
+# Making symbolic link to our binary file from /user/share/
+ln -s /usr/share/kbe/kbe ./kbe/usr/bin/kbe
+
 echo -en "\033[37;1;41mMake *.deb pachage...\033[0m\n"
 cd ./kbe
-md5deep -r -l opt > ./DEBIAN/md5sums
+md5deep -r -l usr/share > ./DEBIAN/md5sums
 cd ..
 fakeroot dpkg-deb --build kbe
 
