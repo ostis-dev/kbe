a14e10b5f6493e8046cfc7adfb07d2ab6a837822
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-02-22 14:18:10 +0300

    Make lintian happy

--- tools/make-deb.sh
+++ tools/make-deb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+VERSION=0.2.1
+ARCHITECTURE=$(dpkg-architecture -qDEB_BUILD_ARCH)
+
+sed -i '/Version/s|: .*$|: '"$VERSION"'|' DEBIAN/control
+sed -i '/Architecture/s|: .*$|: '"$ARCHITECTURE"'|' DEBIAN/control
+
 echo -en "\033[37;1;41mCheck dependencies...\033[0m\n"
 if ! dpkg -l | grep md5deep
 then
@@ -28,10 +34,13 @@ cmake CMakeLists.txt
 make
 
 echo -en "\033[37;1;41mCreate temporary directory for package and copy required files...\033[0m\n"
-cd ./tools/
+# Back to that dir
+cd -
 
 # Making catalog structure 
 mkdir -p ./kbe/usr/share/kbe
+mkdir -p ./kbe/usr/share/doc/kbe
+mkdir -p ./kbe/usr/lib/kbe
 mkdir -p ./kbe/usr/bin
 mkdir -p ./kbe/usr/share/applications
 mkdir -p ./kbe/usr/share/pixmaps
@@ -39,18 +48,30 @@ mkdir -p ./kbe/usr/share/pixmaps
 # Copying needed files into catalog structure
 cp -r ./DEBIAN ./kbe/
 mv -f ../media ./kbe/usr/share/kbe/
-mv -f ../kbe ./kbe/usr/share/kbe/
+mv -f ../kbe ./kbe/usr/lib/kbe/
 cp ./files/kbe.desktop ./kbe/usr/share/applications/
 cp ./files/kbe.xpm ./kbe/usr/share/pixmaps/
 
-# Making symbolic link to our binary file from /user/share/
-ln -s /usr/share/kbe/kbe ./kbe/usr/bin/kbe
+# Making symbolic links
+ln -s ../lib/kbe/kbe ./kbe/usr/bin/kbe
+ln -s ../share/kbe/media ./kbe/usr/lib/kbe/media
+
+# Remove debug and other unneeded info
+strip ./kbe/usr/lib/kbe/kbe
+
+cp changelog ./kbe/usr/share/doc/kbe/
+gzip -9 ./kbe/usr/share/doc/kbe/changelog
+
+cp copyright ./kbe/usr/share/doc/kbe/
 
 echo -en "\033[37;1;41mMake *.deb pachage...\033[0m\n"
 cd ./kbe
-md5deep -r -l usr/share > ./DEBIAN/md5sums
-cd ..
+md5deep -l usr/lib/kbe/kbe > ./DEBIAN/md5sums
+md5deep -r -l usr/share >> ./DEBIAN/md5sums
+cd -
+
 fakeroot dpkg-deb --build kbe
+fakeroot dpkg-name -o kbe.deb
 
 echo -en "\033[37;1;41mRemove temporary data...\033[0m\n"
 rm -r ../CMakeFiles ../sources/*/*.cxx ../sources/*.cxx ./kbe
