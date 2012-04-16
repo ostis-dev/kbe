987489dfe1b6f3a71f19205ca4f8bd4f5c8d62e2
pavalliavonau <paval@open.by>
2012-04-17 00:19:10 +0300

    Fix compilation on 64 bit linux.

--- sources/plugins/scg/scgwindow.cpp
+++ sources/plugins/scg/scgwindow.cpp
@@ -86,8 +86,12 @@ SCgWindow::SCgWindow(const QString& _windowTitle, QWidget *parent) :
     mActionFind(0)//,
 //    mActionMinMap(0)
 {
+    // TODO: correct object name
+    size_t size = sizeof(this);
+    QChar* _char = (QChar*)this;
+    QString str(_char,size);
 
-    setObjectName(QString((quint32)this));
+    setObjectName(QString((QChar*)this, sizeof(this))); // for 64bit systems
 
     mUndoStack = new QUndoStack(this);
     /////////////////////////////////////////////////
