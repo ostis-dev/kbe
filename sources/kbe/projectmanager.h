0384fdca6077573a9bf3bbbe7f66ca461668eefe
mikluke <vmikitich@gmail.com>
2014-04-09 15:34:44 +0300

    Added docs #8

--- sources/kbe/projectmanager.h
+++ sources/kbe/projectmanager.h
@@ -23,6 +23,8 @@ public:
     ProjectManagerView* getTreeView() { return treeView; }
 private:
     static ProjectManagerDockWidget* mInstance;
+
+    //! Content of this widget
     ProjectManagerView *treeView;
 };
 
@@ -52,13 +54,33 @@ public slots:
     void onPropertiesShow();
 
 signals:
+    /*! This signal will send when ProjectManager requests opening some file.
+     * @param Filename
+     */
+
     void openFile(QString);
 
 private:
+    /*! Changes item name
+     * @param item Item which name will be changed
+     * @param newName new name for item
+     */
     void renameItem(ProjectManagerModelItem* item, QString newName);
+
+    /*! Removes item and his childs from localstorage and view
+     * @param item Item which will be removed
+     */
     void permanentRemoveTree(ProjectManagerModelItem* item);
+
+    /*! Creates context menu which appears for item
+     * @param item Item for which menu will be appear
+     */
     void createContextMenu(ProjectManagerModelItem* item);
+
+    //! QTreeView::mousePressEvent with feature that item's focus will be removed if click occured on empty area
     void mousePressEvent(QMouseEvent*);
+
+    //! Opens file if double clicked item's type is file
     void mouseDoubleClickEvent(QMouseEvent *event);
 
 private:
--- sources/kbe/projectmanagermodel.h
+++ sources/kbe/projectmanagermodel.h
@@ -17,6 +17,7 @@ class ProjectManagerModelItem: public QObject
     Q_OBJECT
 
 public:
+    /* Types of item */
     enum ItemType {
         Unknown,
         Project,
@@ -30,7 +31,14 @@ public:
     ProjectManagerModelItem(QString shownName, QString filePath, ItemType type, ProjectManagerModelItem *parent = 0);
     ~ProjectManagerModelItem();
 
+    /*! Appends child in childItems list
+     * @param child Item which will be appeded
+     */
     void appendChild(ProjectManagerModelItem *child);
+
+    /*! Removes child from childItems list
+     * @param child Item which will be removed
+     */
     void removeChild(ProjectManagerModelItem* child);
 
     ProjectManagerModelItem* child(int row);
@@ -38,10 +46,17 @@ public:
     int columnCount() const;
     QVariant data(int column) const;
     int row() const;
+
+    //! Returns item's parent
     ProjectManagerModelItem* parent();
+
+    //! Returns project item which contains this item
     ProjectManagerModelItem* getProjectItem();
 
+    //! Returns absolute file path of current item
     QString getAbsoluteFilePath();
+
+    //! Returns absolute path to directory which contains current item
     QString getAbsoluteFileDir();
 
     ItemType getItemType() const { return type; }
@@ -53,13 +68,21 @@ public:
     void setModified(bool val) { m_isModified = val; }
 
 private:
+    //! Flag which indicates that item is modified
     bool m_isModified;
 
+    //! Pointer to parent
     ProjectManagerModelItem *parentItem;
+    //! List of item's child
     QList<ProjectManagerModelItem*> childItems;
 
+    //Object name contains full path to item and shown name
+
+    //! Name which will be shown in view
     QVariant shownName;
+    //! Relative(for files) or Absolute(for projects) path
     QString filePath;
+    //! Type of item
     ItemType type;
 };
 
@@ -73,18 +96,56 @@ public:
     ProjectManagerModel(QObject *parent);
     ~ProjectManagerModel();
 
+    /*! Adds child to model
+     * @param shownName Item's name which contain all filters and project name. Finally will be shown name without adds
+     * @param filePath Relative(for files) or Absolute(for projects) path to real file
+     * @param type Item's type
+     * @param rootItem Model's root item
+     */
     ProjectManagerModelItem* addChild(const QString shownName, const QString filePath, ProjectManagerModelItem::ItemType type, ProjectManagerModelItem* rootItem);
 
+    /*! Removes item from model
+     * @param item which will be removed from model
+     */
     void removeItem(ProjectManagerModelItem* item);
+
+    /*! Removes item from model by objectName
+     * @param itemObjectName Item's objectName
+     */
     void removeItem(QString itemObjectName);
 
+    /*! Inserts item to model
+     * @param shownName Item's name which contain all filters and project name. Finally will be shown name without adds
+     * @param filePath Relative(for files) or Absolute(for projects) path to real file
+     * @param type Item's type
+     */
     void insertItem(const QString shownName, const QString filePath, ProjectManagerModelItem::ItemType type);
+
+    /*! Inserts project item to model
+     * @param item Project item which will be inserted in model
+     */
     void insertProject(ProjectManagerModelItem* item);
 
+
+    //! Returns model's root item
     ProjectManagerModelItem* getRootItem();
+
+    /*! Returns item of parent by filePath
+     * @param filePath Path to item's file on local storage
+     * @param parent item which seek to item
+     */
     ProjectManagerModelItem* getItemByFilePath(QString filePath, ProjectManagerModelItem* parent);
 
+
+    /*! Loads project to model by filePath
+     * @param filePath Path to project file
+     */
     void loadProject(QString filePath);
+
+    /*! Saves project from model
+    * @param project Project which will be saved
+    * @param filePath Path to file in which will be saved project
+     */
     void saveProject(ProjectManagerModelItem* project,QString filePath=0);
 
     QStringList mimeTypes() const;
@@ -93,8 +154,10 @@ public:
         Qt::DropAction action, int row, int column, const QModelIndex &parent);
     Qt::DropActions supportedDropActions() const;
 
-    QVariant data(const QModelIndex &index, int role) const;
+    //! Converts index to PrijectManagerModelItem
     ProjectManagerModelItem* getItem(const QModelIndex &index) const;
+
+    QVariant data(const QModelIndex &index, int role) const;
     Qt::ItemFlags flags(const QModelIndex &index) const;
     QVariant headerData(int section, Qt::Orientation orientation,
                         int role = Qt::DisplayRole) const;
@@ -105,10 +168,23 @@ public:
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
 private:
+
+    /*! Recoursivly saves item with childs to stream
+    * @param item Item which be saved
+    * @param wStream Stream in which item will be saved
+     */
     void saveChilds(ProjectManagerModelItem* item, QXmlStreamWriter& wStream);
+
+
+    /*! Creates path(filters) in model and returns pointer for tale
+    * @param path Path which will be created in model
+    * @param parent Item which will be used to find or create next filter
+    * \return Pointer to last created item (path's tale)
+     */
     ProjectManagerModelItem* getPath(QStringList path, ProjectManagerModelItem *parent);
 
 private:
+    //! Model's root item
     ProjectManagerModelItem *rootItem;
 
 };
