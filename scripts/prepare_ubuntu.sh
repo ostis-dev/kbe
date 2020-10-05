d2f9b3cf4505d529d8b566a918c8a43f96bc7241
Alexander Yurkov <zioba1404@gmail.com>
2020-10-05 11:30:27 +0300

    updated readme

--- scripts/prepare_ubuntu.sh
+++ scripts/prepare_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install qtbase5-dev qttools5-dev qttools5-dev-tools qtquick1-5-dev qtscript5-dev libqt5xmlpatterns5-dev libqt5svg5-dev libqt5webkit5-dev antlr libantlr3c-dev antlr3
+sudo apt-get install qtbase5-dev qttools5-dev qttools5-dev-tools qtscript5-dev libqt5xmlpatterns5-dev libqt5svg5-dev libqt5webkit5-dev antlr libantlr3c-dev antlr3
