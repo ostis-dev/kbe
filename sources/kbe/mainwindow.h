fe5d439d04044e174b3514f79220264da352d0f8
Alexander Yurkov <zioba1404@gmail.com>
2021-03-16 16:22:50 +0300

    remove old method prototype

--- sources/kbe/mainwindow.h
+++ sources/kbe/mainwindow.h
@@ -172,7 +172,6 @@ public slots:
     void onFileSaveAs(QWidget* window = 0);
     void onFileSaveAll();
     void onFileExportToImage();
-    void onExportAllFilesToImages();
     void onFileExit();
 
     void onViewSettings();
